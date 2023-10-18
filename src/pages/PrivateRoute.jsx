dda2672414222369aae7b075c57a61ad673d20d2
raja <memonabdulrehman250@gmail.com>
2023-10-18 21:39:08 +0500

    fix

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,13 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
+
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useAuth0()
+  
   const navigate = useNavigate()
-  if (isAuthenticated) {
+  useEffect(()=>{
+    if(!isAuthenticated){
+      navigate("/login")
+    }
+  },[])
+  // if (isAuthenticated) {
     return children
-  }
-  return navigate({to:"/login"})
+  // }
 
 };
 export default PrivateRoute;
